fb005b137fa0f372488c726a9762495d3f8a46d2
mariaptv <mariaptellezv@gmail.com>
2024-09-13 20:59:56 -0500

    Add solution class

--- turtle_race.py
+++ turtle_race.py
@@ -23,9 +23,7 @@ if user_bet:
     is_race_on = True
 
 while is_race_on:
-
     for turtle in all_turtles:
-
         if turtle.xcor() > 230:
             is_race_on = False
             winning_color = turtle.pencolor()
